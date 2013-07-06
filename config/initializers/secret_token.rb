f0ed5710cb0b5096a41d76052ebbecb0165ddbe7
suyesh Bhandari <suyeshb@gmail.com>
2013-07-06 16:39:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trollmemaybe::Application.config.secret_token = '38aa200878c7c6b2bc7c3a0b7befa04b4937f074ba415e984c852931725403ec92773fffb8b74ff5369e37f7794a2276f47bebb9271be1bd6f1f67214eeb8832'
